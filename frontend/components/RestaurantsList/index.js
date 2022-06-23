09135cc37116575db007af2646c633accfec42c3
yoshinoseiji <sij.ysn25@gmail.com>
2022-06-23 23:36:02 +0900

    restaurantsの表示を実装

---
+++ frontend/components/RestaurantsList/index.js
@@ -0,0 +1,82 @@
+import { Row, Col, Card, CardImg, CardBody, CardTitle } from "reactstrap";
+import Link from "next/link";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const query = gql`
+  {
+    restaurants {
+      data {
+        id
+        attributes {
+          name
+          description
+          image {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const RestaurantList = (props) => {
+  const { loading, error, data } = useQuery(query);
+  if (error) return "レストランの読み込みに失敗しました";
+  if (loading) return "読み込み中";
+  if (data) {
+    const searchQuery = data.restaurants.data.filter((restaurant) =>
+      restaurant.attributes.name.toLowerCase().includes(props.search)
+    );
+    return (
+      <Row>
+        {searchQuery.map((res) => (
+          <Col xs="6" sm="4" key={res.id}>
+            <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
+              <CardImg
+                src={`${process.env.NEXT_PUBLIC_API_URL}${res.attributes.image.data[0].attributes.url}`}
+                top={true}
+                style={{ height: 250 }}
+              />
+              <CardBody>
+                <CardTitle>{res.attributes.name}</CardTitle>
+                <CardTitle>{res.attributes.description}</CardTitle>
+              </CardBody>
+              <div className="card-footer">
+                <Link href="/restaurants?id=1" as="/restaurants/1">
+                  <a className="btn btn-primary">もっと見る</a>
+                </Link>
+              </div>
+            </Card>
+          </Col>
+        ))}
+
+        <style jsx>
+          {`
+            a {
+              color: white;
+            }
+            a:link {
+              text-decoration: none;
+              color: white;
+            }
+            a:hover {
+              color: white;
+            }
+            .card-columns {
+              column-count: 3;
+            }
+          `}
+        </style>
+      </Row>
+    );
+  } else {
+    return <h1>レストランが見つかりませんでした。</h1>;
+  }
+};
+
+export default RestaurantList;
---
+++ frontend/pages/index.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Col, Input, InputGroup, InputGroupText, Row } from "reactstrap";
+import RestaurantList from "../components/RestaurantsList";
+
+const index = () => {
+  const [query, setQuery] = useState("");
+  return (
+    <div className="container-fluid">
+      <Row>
+        <Col>
+          <div className="search">
+            <InputGroup>
+              <InputGroupText>探す</InputGroupText>
+              <Input
+                placeholder="レストラン名を検索してください"
+                onChange={(e) => setQuery(e.target.value.toLocaleLowerCase())}
+              />
+            </InputGroup>
+          </div>
+          <RestaurantList search={query} />
+        </Col>
+      </Row>
+      <style jsx>
+        {`
+          .search {
+            margin: 20px;
+            width: 500px;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default index;
